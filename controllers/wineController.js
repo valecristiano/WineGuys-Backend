3853f3dd5f592656b7d2b5a6f259e2ec53c3b463
valecristiano <valeriamdcristiano@gmail.com>
2026-03-31 14:22:47 +0300

    add endpoint categoria mrk + fix url image

--- controllers/wineController.js
+++ controllers/wineController.js
@@ -15,15 +15,17 @@ const index = (req, res) => {
   });
 };
 
-// INDEX - ritorna tutti i vini colla loro categoria
+// INDEX - Cateforia Vini in promo
 const indexPromo = (req, res) => {
   // query che prende tutti i prodotti e fa JOIN conlle categorie
   const sql = `
-   SELECT p.*, c.category_name
-   FROM products p
-   JOIN mrk_categories c 
-   ON p.category_id = c.id
-   WHERE c.id = 1
+   SELECT 
+    p.*, 
+    CONCAT('http://localhost:3000/wines/', p.img) AS img_url,
+    c.category_name
+  FROM products p
+  JOIN mrk_categories c ON p.category_id = c.id
+  WHERE c.id = 3
    `;
 
   // esegue la query sul database
@@ -35,6 +37,73 @@ const indexPromo = (req, res) => {
   });
 };
 
+// INDEX - categoria vini premiati
+const indexPremi = (req, res) => {
+  // query che prende tutti i prodotti e fa JOIN conlle categorie
+  const sql = `
+   SELECT 
+    p.*, 
+    CONCAT('http://localhost:3000/wines/', p.img) AS img_url,
+    c.category_name
+  FROM products p
+  JOIN mrk_categories c ON p.category_id = c.id
+  WHERE c.id = 2
+   `;
+
+  // esegue la query sul database
+  connection.query(sql, (err, results) => {
+    // se c'è un errore lo manda al client
+    if (err) return handleFailQuery(err, res);
+    // se va bene manda i risultati al client
+    res.json(results);
+  });
+};
+
+// INDEX - categoria vini più venduti
+const indexPiuVenduti = (req, res) => {
+  // query che prende tutti i prodotti e fa JOIN conlle categorie
+  const sql = `
+   SELECT 
+    p.*, 
+    CONCAT('http://localhost:3000/wines/', p.img) AS img_url,
+    c.category_name
+  FROM products p
+  JOIN mrk_categories c ON p.category_id = c.id
+  WHERE c.id = 1
+   `;
+
+  // esegue la query sul database
+  connection.query(sql, (err, results) => {
+    // se c'è un errore lo manda al client
+    if (err) return handleFailQuery(err, res);
+    // se va bene manda i risultati al client
+    res.json(results);
+  });
+};
+
+// INDEX - categoria vini di primavera
+const indexPrimavera = (req, res) => {
+  // query che prende tutti i prodotti e fa JOIN conlle categorie
+  const sql = `
+   SELECT 
+    p.*, 
+    CONCAT('http://localhost:3000/wines/', p.img) AS img_url,
+    c.category_name
+  FROM products p
+  JOIN mrk_categories c ON p.category_id = c.id
+  WHERE c.id = 4
+   `;
+
+  // esegue la query sul database
+  connection.query(sql, (err, results) => {
+    // se c'è un errore lo manda al client
+    if (err) return handleFailQuery(err, res);
+    // se va bene manda i risultati al client
+    res.json(results);
+  });
+};
+
+//SHOW
 const show = (req, res) => {
   // prende lo slug dall'url
   const { slug } = req.params;
@@ -59,4 +128,4 @@ const store = (req, res) => {
   res.json({ message: "Vino aggiunto" });
 };
 
-module.exports = { index, indexPromo, show, store };
+module.exports = { index, indexPromo, indexPremi, indexPiuVenduti, indexPrimavera, show, store };
